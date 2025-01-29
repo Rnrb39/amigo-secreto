c756c5707cd1e6b92e1c25700f2e4e6d491efc51
Rene Ramirez <r.ramirezbustamante@gmail.com>
2025-01-29 12:47:39 -0300

    actualiza juego

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-console.log('empezar desafio')
+let listaAmigos = [];
+
+// Verifica si se ingreso u nombre en la caja
+function validar(){
+    let amigo1 = document.getElementById('amigo').value;
+    console.log(amigo1);
+    if ( amigo1 == "" ){
+        alert('Por favor ingrese un nombre');
+        return;
+    }
+};
+
+// Asigna texto al HTML desde js
+function asignarTexto(etiqueta, texto) {
+    let etiquetaHtml = document.querySelector(etiqueta);
+    etiquetaHtml.innerHTML = texto;
+    return;
+
+}
+
+function agregarAmigo() {
+   validar();
+   let amigo = document.getElementById('amigo').value;
+   listaAmigos.push(amigo);
+   limpiarTexto();
+   listaAmigos.forEach((element) => console.log(element));
+   asignarTexto('ul', listaAmigos );
+   return;
+   
+}
+
+function limpiarTexto() {
+    let textoCaja = document.getElementById("amigo");
+    textoCaja.value = '';
+
+
+}
+
+function sortearAmigo()  {
+    let indiceLista = Math.floor(Math.random() * listaAmigos.length );
+    document.getElementById("resultado").innerHTML = `Tu amigo secreto es "${listaAmigos[indiceLista]}"`;
+    console.log(indiceLista);
+   
+
+}
+
+
+
+/*function limpiarTexto() {
+    let limpiarTexto = document.getElementById('amigo');
+    limpiarTexto.value = '';
+
+}*/
+
+asignarTexto('h1', 'Juego del Amigo Secreto');
+asignarTexto('h2', 'ingresa los nombres de tus amigos');
+
